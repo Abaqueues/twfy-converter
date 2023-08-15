5a6ffbb3a096f3b7802baef66ede98d565f2737c
James Cornall <jjc257@student.bham.ac.uk>
2023-08-15 18:50:29 +0100

    "Add comments"

--- TWFYPreprocessor.py
+++ TWFYPreprocessor.py
@@ -2,11 +2,15 @@ import xml.etree.ElementTree as ET
 import json
 import os
 
+# Set folder locations for XML input and JSON output
 xml_folder = 'E:\\Documents\\Education\\PG University of Birmingham\\MSc Computer Science\\Summer Semester\\MSc Projects\\Project Files\\Dataset\\xml'
 json_output_folder = 'E:\\Documents\\Education\\PG University of Birmingham\\MSc Computer Science\\Summer Semester\\MSc Projects\\Project Files\\Dataset\\json' 
 
+# Check whether the input and output folders exist
+os.makedirs(xml_folder, exist_ok=True)
 os.makedirs(json_output_folder, exist_ok=True)
 
+# Parse XML data 
 for xml_file in os.listdir(xml_folder):
     if xml_file.endswith('.xml'):
         xml_path = os.path.join(xml_folder, xml_file)
@@ -18,14 +22,14 @@ for xml_file in os.listdir(xml_folder):
         
         data = []
         
+        # Iterates through each element, collating any text speech content
         for element in root:
             item = {}
             for sub_element in element:
                 item[sub_element.tag] = sub_element.text
             if item != {}:
                 data.append(item)
-            
+        
+        # Adds the collected data to the .json file
         with open(json_path, 'w') as json_file:
             json.dump(data, json_file, indent=4)
-            
-        
