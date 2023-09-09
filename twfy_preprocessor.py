03a337d3e30e8777e3fdc0957e1c60256fcc81be
James Cornall <jjc257@student.bham.ac.uk>
2023-09-09 23:56:22 +0100

    Modified file paths for portability

--- twfy_preprocessor.py
+++ twfy_preprocessor.py
@@ -3,14 +3,15 @@ import json
 import os
 import re
 
-# Set folder locations for XML input, JSON output, and merged intermediate XML files
-_XML_FOLDER = 'E:\\Documents\\Education\\PG University of Birmingham\\MSc Computer Science\\Summer Semester\\MSc Projects\\Project Files\\Dataset\\final\\xml'
-_JSON_OUTPUT_FOLDER = 'E:\\Documents\\Education\\PG University of Birmingham\\MSc Computer Science\\Summer Semester\\MSc Projects\\Project Files\\Dataset\\final\\json' 
-_MERGED_XML_FOLDER = 'E:\\Documents\\Education\\PG University of Birmingham\\MSc Computer Science\\Summer Semester\\MSc Projects\\Project Files\\Dataset\\final\\merged'
+# Set folder paths for XML input, JSON output, and merged intermediate XML files
+_XML_INPUT_FOLDER = ".\\TWFYScraper\\xml\\"
+_JSON_OUTPUT_FOLDER = ".\\TWFYPreprocessor\\json\\"
+_MERGED_XML_FOLDER = ".\\TWFYPreprocessor\\merged\\"
 
 # Check whether the input and output folders exist
-os.makedirs(_XML_FOLDER, exist_ok=True)
-# os.makedirs(_JSON_OUTPUT_FOLDER, exist_ok=True)
+os.makedirs(_XML_INPUT_FOLDER, exist_ok=True)
+os.makedirs(_JSON_OUTPUT_FOLDER, exist_ok=True)
+os.makedirs(_MERGED_XML_FOLDER, exist_ok=True)
 
 # Define filename date regular expression pattern
 _DATE_PATTERN = r'\d{4}-\d{2}-\d{2}'
@@ -19,13 +20,13 @@ _DATE_PATTERN = r'\d{4}-\d{2}-\d{2}'
 def match_file_dates():
     previous_matching_files = []
     merge_list = []
-    for xml_file in os.listdir(_XML_FOLDER):
+    for xml_file in os.listdir(_XML_INPUT_FOLDER):
         match = re.search(_DATE_PATTERN, xml_file)
-        matching_files = [xml_file for xml_file in os.listdir(_XML_FOLDER) if match.group() in xml_file]
+        matching_files = [xml_file for xml_file in os.listdir(_XML_INPUT_FOLDER) if match.group() in xml_file]
         if matching_files != previous_matching_files:
             merge_list.append(matching_files)
         previous_matching_files = matching_files
-    # print("Files to merge:", *merge_list, sep = "\n- ")
+    print("Files to merge:", *merge_list, sep = "\n- ")
     return merge_list
 
 # Merge data from XML files in each merge_list index in a new XML file
@@ -35,20 +36,20 @@ def merge_xml_data(file_list):
         match = re.search(_DATE_PATTERN, files[0])
         merged_root = ET.Element("merged_data" + match.group())
         for xml_file in files:
-            xml_path = os.path.join(_XML_FOLDER, xml_file)
+            xml_path = os.path.join(_XML_INPUT_FOLDER, xml_file)
             with open(xml_path, "r", encoding="unicode_escape") as file:
                 xml_data = file.read()
                 root = ET.fromstring(xml_data)
                 for element in root:
                     merged_root.append(element)           
         merged_file = ET.ElementTree(merged_root)
-        merged_file.write(_MERGED_XML_FOLDER + "\\merged" + match.group() + ".xml")
+        merged_file.write(_MERGED_XML_FOLDER + "merged" + match.group() + ".xml")
         merged_file_list.append("merged" + match.group() + ".xml")
-    # print("\nMerged files:", *merged_file_list, sep = "\n- ")
+    print("\nMerged files:", *merged_file_list, sep = "\n- ")
 
 # Function to check whether an XML file contains a division
 def contains_division(xml_file):
-    tree = ET.parse(_MERGED_XML_FOLDER + "\\" + xml_file)
+    tree = ET.parse(_MERGED_XML_FOLDER + xml_file)
     root = tree.getroot()
     element = root.find("division")
     if element is not None:
@@ -65,8 +66,8 @@ def report_division_files():
             division_true_list.append(file)
         else:
             division_false_list.append(file)
-    # print("\nFiles containing division:", *division_true_list, sep = "\n- ")
-    # print("\nFiles missing division:", *division_false_list, sep = "\n- ")
+    print("\nFiles containing division:", *division_true_list, sep = "\n- ")
+    print("\nFiles missing division:", *division_false_list, sep = "\n- ")
     return division_true_list
 
 # Function that assigns division vote labels to XML speech tags
@@ -74,7 +75,7 @@ def assign_vote_labels(file_list):
     for file in file_list:
         print(file)
         divisions = {}
-        tree = ET.parse(_MERGED_XML_FOLDER + "\\" + file)
+        tree = ET.parse(_MERGED_XML_FOLDER + file)
         root = tree.getroot()
         for division in root.iter("division"):
             match = re.search(r'\b(\d{4}-\d{2}-\d{2}[a-zA-Z]?)\b', division.attrib["id"])
@@ -101,7 +102,7 @@ def assign_vote_labels(file_list):
         # Iterates through each element, collating any text speech content in a JSON format
         match = re.search(_DATE_PATTERN, file)
         json_filename = match.group(0) + ".json"
-        json_path = os.path.join(_JSON_OUTPUT_FOLDER + "\\", json_filename)
+        json_path = os.path.join(_JSON_OUTPUT_FOLDER, json_filename)
         data = []
         for element in root:
             if element.tag in ["speech"] and ("person_id" in element.attrib or "speakerid" in element.attrib):
@@ -130,5 +131,5 @@ def assign_vote_labels(file_list):
         with open(json_path, "w", encoding="utf-8") as file:
             file.write(json_data)
             
-# merge_xml_data(match_file_dates())
+merge_xml_data(match_file_dates())
 assign_vote_labels(report_division_files())
